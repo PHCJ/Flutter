6df40faa466bc8917f9b6fb0add097f8a2175ba4
Paulo <paulo.jesus2@fatec.sp.gov.br>
2022-11-04 09:05:50 -0300

    deletar add

--- sqlite_teste/lib/main.dart
+++ sqlite_teste/lib/main.dart
@@ -6,7 +6,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,14 +15,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter SQLite'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHOmePage extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
   final dbHelp = DatabaseHelper.instance;
-  MyHOmePage({super.key});
+  MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,7 +74,7 @@ class MyHOmePage extends StatelessWidget {
         )));
   }
 
-  void _insert() async {
+  void _inserir() async {
     Map<String, dynamic> row = {
       DatabaseHelper.columnNome: 'Paulo',
       DatabaseHelper.columnIdade: 26
@@ -81,90 +83,27 @@ class MyHOmePage extends StatelessWidget {
     print('linha inserida id: $id');
   }
 
-  void _consultar() async {}
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _consultar() async {
+    final todasLinhas = await dbHelp.queryAllRows();
+    print('Consulta todas as linhas');
+    for (var row in todasLinhas) {
+      print(row);
+    }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void _atualizar() async {
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnId: 1,
+      DatabaseHelper.columnNome: 'Jose',
+      DatabaseHelper.columnIdade: 23
+    };
+    final linhasAfetadas = await dbHelp.update(row);
+    print('atualizadas $linhasAfetadas linhas');
+  }
+
+  void _deletar() async {
+    final id = await dbHelp.queryRowCount();
+    final linhaDeletada = await dbHelp.delete(id!);
+    print('Deletada(s) $linhaDeletada linha(s): linha $id');
   }
 }
